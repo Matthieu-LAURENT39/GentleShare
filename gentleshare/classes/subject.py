9206c10f52c713e8d184aa609a2676d2a9a87e0e
Matthieu LAURENT <matthieu.laurent69@proton.me>
2024-02-21 09:00:44 +0100

    Add the subject and education_level enums

---
+++ gentleshare/classes/subject.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class Subject(Enum):
+    """Enum class for the subjects that can be taught in the school."""
+
+    MATHS = "maths"
+    FRENCH = "french"
+    SPANISH = "spanish"
+    ENGLISH = "english"
+    PHYSICS = "physics"
+    CHEMISTRY = "chemistry"
+    BIOLOGY = "biology"
+    HISTORY = "history"
+    GEOGRAPHY = "geography"
+    ECONOMICS = "economics"
+    ECONOMY = "economy"
