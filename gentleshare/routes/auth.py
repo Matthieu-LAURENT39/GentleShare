ce2a1ca6164cce5216a8c422e0ed3435cf07b5cb
Matthieu LAURENT <matthieu.laurent69@proton.me>
2024-02-22 14:02:39 +0100

    Add auth The code is not that clean since i bet a friend i could do it in under 5 minutes

--- gentleshare/routes/auth.py
+++ gentleshare/routes/auth.py
@@ -1,59 +1,44 @@
-from flask import render_template, request, redirect, url_for, flash, session
-from markupsafe import escape
-from flask_login import login_user, current_user, logout_user
+from flask import render_template, request, redirect, url_for, flash
+from flask_login import login_user
 from . import main
 from ..database import User, db
 from sqlalchemy.exc import IntegrityError
-from loguru import logger
-from ..forms import LoginForm, RegisterForm
-from ..classes import FlashCategory
 
 
 @main.route("/login", methods=["GET", "POST"])
 def login() -> str:
-    """Login route"""
-    if current_user.is_authenticated:
-        flash("You are already logged in", FlashCategory.WARNING)
-        return redirect(url_for("main.index"))
+    """Login route
 
-    form = LoginForm()
+    Returns:
+        str: The login page
+    """
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
 
-    if form.validate_on_submit():
-        username = form.username.data
-        password = form.password.data
-
-        user: User = User.query.filter_by(username=username).first()
+        user = User.query.filter_by(username=username).first()
 
         if user and user.check_password(password):
-            if user.totp_enabled:
-                logger.info(f"Sending '{username}' to TOTP validation")
-                session["awaiting_totp"] = user.username
-                return render_template("totp.jinja")
-
-            logger.info(f"Logging in user '{username}'")
             login_user(user)
             return redirect(url_for("main.index"))
 
-        logger.info(f"Failed login attempt for user '{username}'")
-        flash("Invalid username or password", FlashCategory.ERROR)
+        flash("Invalid username or password", "error")
 
-    return render_template("login.jinja", form=form)
+    return render_template("login.jinja")
 
 
 @main.route("/register", methods=["GET", "POST"])
 def register() -> str:
-    """Register route"""
-    if current_user.is_authenticated:
-        flash("You are already logged in", FlashCategory.WARNING)
-        return redirect(url_for("main.index"))
+    """Register route
 
-    form = RegisterForm()
+    Returns:
+        str: The register page
+    """
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
 
-    if form.validate_on_submit():
-        username = form.username.data
-        password = form.password.data
-
-        logger.info(f"Registering user '{username}'")
+        print(username, password)
 
         user = User(username=username)
         user.set_password(password)
@@ -63,46 +48,9 @@ def register() -> str:
             db.session.commit()
         # If the user already exists
         except IntegrityError:
-            db.session.rollback()
-            flash(
-                f"Username '{escape(username)}' is already taken", FlashCategory.ERROR
-            )
-            return render_template("register.jinja", form=form)
+            flash(f"Username is already taken", "error")
+            return render_template("register.jinja")
 
-        login_user(user)
-        flash("User created", FlashCategory.SUCCESS)
-        return redirect(url_for("main.index"))
+        flash("User created", "success")
 
-    return render_template("register.jinja", form=form)
-
-
-@main.route("/logout")
-def logout() -> str:
-    if current_user.is_authenticated:
-        logout_user()
-        flash("You have been logged out", FlashCategory.SUCCESS)
-    else:
-        flash("You can't log out without being logged in", FlashCategory.WARNING)
-    return redirect(url_for("main.index"))
-
-
-@main.route("/totp", methods=["GET", "POST"])
-def validate_totp() -> str:
-    # This can't be forged, it's signed by the server
-    username = session.get("awaiting_totp")
-    if not username:
-        return redirect(url_for("main.login"))
-
-    if request.method == "POST":
-        totp = request.form.get("totp")
-        user: User = User.query.filter_by(username=username).first()
-
-        if user.check_totp(totp):
-            logger.info(f"Logging in user '{username}', after TOTP validation")
-            login_user(user)
-            session.pop("awaiting_totp")
-            return redirect(url_for("main.index"))
-
-        flash("Invalid TOTP code", FlashCategory.ERROR)
-
-    return render_template("totp.jinja")
+    return render_template("register.jinja")
