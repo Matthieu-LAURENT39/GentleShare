ae5cee90df3615396d7e44c2eeeb03e3339d6302
Matthieu LAURENT <matthieu.laurent69@proton.me>
2024-02-23 14:41:50 +0100

    Add display name to education_level and subject

--- gentleshare/classes/education_level.py
+++ gentleshare/classes/education_level.py
@@ -8,3 +8,12 @@ class EducationLevel(Enum):
     MIDDLE = "middle"
     HIGH = "high"
     UNIVERSITY = "university"
+
+    @property
+    def display_name(self) -> str:
+        return {
+            EducationLevel.ELEMENTARY: "Elementary school",
+            EducationLevel.MIDDLE: "Middle school",
+            EducationLevel.HIGH: "High school",
+            EducationLevel.UNIVERSITY: "University",
+        }[self]
