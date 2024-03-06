4e50219b30df0a7e89c315ce152bef59d067c762
Matthieu LAURENT <matthieu.laurent69@proton.me>
2024-03-06 17:08:25 +0100

    Add pretty validation

--- gentleshare/forms/profile.py
+++ gentleshare/forms/profile.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired
 
 
 class ProfileForm(FlaskForm):
     display_name = StringField("Nom", validators=[DataRequired()])
-    about = StringField("A propos de moi")
+    about = TextAreaField("A propos de moi")
