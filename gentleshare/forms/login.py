2375206bad12de4ee4811bb906859f39295ae084
Matthieu LAURENT <matthieu.laurent69@proton.me>
2024-03-06 17:34:27 +0100

    Hide password in login form

--- gentleshare/forms/login.py
+++ gentleshare/forms/login.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
     username = StringField("Nom d'utilisateur", validators=[DataRequired()])
-    password = StringField("Mot de passe", validators=[DataRequired()])
+    password = PasswordField("Mot de passe", validators=[DataRequired()])
