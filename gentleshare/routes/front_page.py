8603265f262eb2dcb9ad6dd826917bd88afad510
Matthieu LAURENT <matthieu.laurent69@proton.me>
2024-03-06 10:45:38 +0100

    Fix profile

--- gentleshare/routes/front_page.py
+++ gentleshare/routes/front_page.py
@@ -8,3 +8,8 @@ from . import main
 def index():
     file_list: list[File] = File.query.all()
     return render_template("index.jinja", file_list=file_list)
+
+
+@main.route("/profile")
+def profile():
+    return render_template("profile.jinja")
