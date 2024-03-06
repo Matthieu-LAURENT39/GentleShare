6684e8378c153998378dc2dfa653a8dcf35541e6
leoozzz <leonardo.heron@etu.univ-st-etienne.fr>
2024-03-06 10:35:30 +0100

    Co-authored-by: Quentin Saillard <squ1ddd@users.noreply.github.com> Co-authored-by: Loris1601 <Loris1601@users.noreply.github.com> Co-authored-by: Noé Baiocchi <koro-code@users.noreply.github.com>

--- gentleshare/routes/front_page.py
+++ gentleshare/routes/front_page.py
@@ -6,3 +6,6 @@ from . import main
 @main.route("/")
 def index():
     return render_template("acceuil.jinja")
+@main.route("/profile_page")
+def profile_page():
+    return render_template("private_profile.jinja")
