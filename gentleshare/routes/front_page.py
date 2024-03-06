d4e3fa71d1193b13e67bd40fc72e8949703af038
Matthieu LAURENT <matthieu.laurent69@proton.me>
2024-03-06 17:47:13 +0100

    Add basic course upload

--- gentleshare/routes/front_page.py
+++ gentleshare/routes/front_page.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from flask_login import current_user
 
-from ..database import File, User
+from ..database import File, User, Course
 from . import main
 from ..forms import ProfileForm
 from ..database import db
@@ -10,7 +10,8 @@ from ..database import db
 @main.route("/")
 def index():
     file_list: list[File] = File.query.order_by(File.uploaded_at.desc()).all()
-    return render_template("index.jinja", file_list=file_list)
+    course_list: list[Course] = Course.query.order_by(Course.id.desc()).all()
+    return render_template("index.jinja", file_list=file_list, course_list=course_list)
 
 
 @main.route("/profile", methods=["GET", "POST"])
