5c224227ec80cd8ed9b0c7f8cb5b566cc384a5ca
leoozzz <leonardo.heron@etu.univ-st-etienne.fr>
2024-03-14 16:05:15 +0100

    Cards
    Co-authored-by: Noe Baiocchi <nbaiocchi@users.noreply.github.com>
    Co-authored-by: Loris1601 <Loris1601@users.noreply.github.com>
    Co-authored-by: Quentin Saillard <squ1ddd@users.noreply.github.com>

--- gentleshare/routes/files.py
+++ gentleshare/routes/files.py
@@ -2,7 +2,7 @@ from flask import render_template, request, redirect, url_for, flash, session
 from markupsafe import escape
 from flask_login import login_user, current_user, logout_user
 from . import main
-from ..database import User, db, File
+from ..database import User, db, File, Course
 from sqlalchemy.exc import IntegrityError
 from loguru import logger
 from ..forms import LoginForm, RegisterForm
@@ -12,3 +12,9 @@ def myfiles() -> str:
     file_list: list[File] = File.query.order_by(File.uploaded_at.desc()).all()
 
     return render_template("my_file.jinja", file_list=file_list)
+
+@main.route("/mycourses", methods=["GET", "POST"])
+def mycourses() -> str:
+    course_list: list[Course] = Course.query.order_by(Course.id.desc()).all()
+
+    return render_template("my_courses.jinja", course_list=course_list)
