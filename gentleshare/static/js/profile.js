bf9cebc2d686112830532df08319e11515ec82d2
leoozzz <leonardo.heron@etu.univ-st-etienne.fr>
2024-03-14 17:47:13 +0100

    ptit push pour le M

--- gentleshare/static/js/profile.js
+++ gentleshare/static/js/profile.js
@@ -1,4 +1,8 @@
-const totp_qr_code = document.getElementById('totp-qr-code');
-totp_qr_code.addEventListener('click', () => {
-    totp_qr_code.classList.toggle("blurred");
+document.addEventListener('DOMContentLoaded', () => {
+    const totp_qr_code = document.getElementById('totp-qr-code');
+    if (totp_qr_code) {
+        totp_qr_code.addEventListener('click', () => {
+            totp_qr_code.classList.toggle("blurred");
+        });
+    }
 });
