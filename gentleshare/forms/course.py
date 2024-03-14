cba87535b7ad7950d8bb5a588d2ff8c2606d6132
Matthieu LAURENT <matthieu.laurent69@proton.me>
2024-03-14 15:40:41 +0100

    Add course creation form

--- gentleshare/forms/course.py
+++ gentleshare/forms/course.py
@@ -1,39 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import SelectField, StringField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms import SelectField, StringField
+from wtforms.validators import DataRequired
 
 from ..classes import EducationLevel, Subject
 
 
 class AddCourseForm(FlaskForm):
-    title = StringField(
-        "Titre",
-        validators=[
-            DataRequired(),
-            Length(
-                min=3,
-                max=100,
-                message="Le titre doit faire entre %(min)d et %(max)d caractères",
-            ),
-        ],
-    )
+    title = StringField("Titre", validators=[DataRequired()])
     """The title of the course"""
-    description = TextAreaField(
-        "Description",
-        validators=[
-            Length(
-                max=500, message="La description doit faire moins de %(max)d caractères"
-            )
-        ],
-    )
+    description = StringField("Description", validators=[DataRequired()])
     """The description of the course. Can contain Markdown."""
 
     education_level = SelectField(
         "Niveau d'éducation",
-        choices=[(level.name, level.display_name) for level in EducationLevel],
+        choices=[(level.name, level.value) for level in EducationLevel],
     )
     """The education level the course is associated with"""
     subject = SelectField(
-        "Matière", choices=[(subject.name, subject.display_name) for subject in Subject]
+        "Matière", choices=[(subject.name, subject.value) for subject in Subject]
     )
     """The subject the course is associated with"""
