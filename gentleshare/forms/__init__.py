be1d3cb47bedc36478ed4b8db85696eb86a206ba
Matthieu LAURENT <matthieu.laurent69@proton.me>
2024-03-14 16:13:11 +0100

    Switch upload_file to WTForms

--- gentleshare/forms/__init__.py
+++ gentleshare/forms/__init__.py
@@ -2,3 +2,4 @@ from .login import LoginForm
 from .register import RegisterForm
 from .profile import ProfileForm
 from .course import AddCourseForm
+from .file import AddFileForm
--- gentleshare/forms/file.py
+++ gentleshare/forms/file.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, StringField, FileField
 from wtforms.validators import DataRequired
-from flask_wtf.file import FileRequired, FileSize
+from flask_wtf.file import FileRequired
 
 from ..classes import EducationLevel, Subject
 from . import AddCourseForm
@@ -9,15 +9,12 @@ from . import AddCourseForm
 
 # For now it's almost the exact same as AddCourseForm, so we just recycle it
 class AddFileForm(AddCourseForm):
+    filename = StringField("Nom du fichier", validators=[DataRequired()])
     file = FileField(
         "Fichier",
         validators=[
             DataRequired(),
             FileRequired(),
-            FileSize(
-                max_size=10 * 1024 * 1024,
-                message="Le fichier est trop gros. Taille max: 10 Mo",
-            ),  # 10 Mo
         ],
     )
     """The file to upload"""
