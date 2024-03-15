e0ffbbfa0c65530c234c608ccb965b780bda2c35
Matthieu LAURENT <matthieu.laurent69@proton.me>
2024-03-15 11:36:02 +0100

    Add validators to all forms

--- gentleshare/forms/course.py
+++ gentleshare/forms/course.py
@@ -1,14 +1,31 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, StringField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 from ..classes import EducationLevel, Subject
 
 
 class AddCourseForm(FlaskForm):
-    title = StringField("Titre", validators=[DataRequired()])
+    title = StringField(
+        "Titre",
+        validators=[
+            DataRequired(),
+            Length(
+                min=3,
+                max=100,
+                message="Le titre doit faire entre %(min)d et %(max)d caractères",
+            ),
+        ],
+    )
     """The title of the course"""
-    description = TextAreaField("Description", validators=[DataRequired()])
+    description = TextAreaField(
+        "Description",
+        validators=[
+            Length(
+                max=500, message="La description doit faire moins de %(max)d caractères"
+            )
+        ],
+    )
     """The description of the course. Can contain Markdown."""
 
     education_level = SelectField(
--- gentleshare/forms/register.py
+++ gentleshare/forms/register.py
@@ -1,9 +1,32 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms.validators import DataRequired, EqualTo, Length
 
 
 class RegisterForm(FlaskForm):
-    username = StringField("Nom d'utilisateur", validators=[DataRequired()])
-    password = PasswordField("Mot de passe", validators=[DataRequired()])
-    password_confirm = PasswordField("Confirmation du mot de passe", validators=[DataRequired(), EqualTo("password", "Passwords must match")])
+    username = StringField(
+        "Nom d'utilisateur",
+        validators=[
+            DataRequired(),
+            Length(
+                min=5,
+                max=25,
+                message="Le nom d'utilisateur doit faire entre %(min)d et %(max)d caractères",
+            ),
+        ],
+    )
+    password = PasswordField(
+        "Mot de passe",
+        validators=[
+            DataRequired(),
+            Length(
+                min=8,
+                max=100,
+                message="Le mot de passe doit faire entre %(min)d et %(max)d caractères",
+            ),
+        ],
+    )
+    password_confirm = PasswordField(
+        "Confirmation du mot de passe",
+        validators=[DataRequired(), EqualTo("password", "Passwords must match")],
+    )
