998260bc6a85835f6099f9476497344450bf3b72
BlackFire_off <96128400+Blackfireoff@users.noreply.github.com>
2024-03-15 15:55:34 +0000

    we can remove fav from UI

--- gentleshare/routes/files.py
+++ gentleshare/routes/files.py
@@ -57,3 +57,37 @@ def addtofav() -> str:
     return redirect(
         url_for("main.list_files")
     )  # Redirigez vers la liste de fichiers après l'ajout aux favoris
+
+@main.route("/rmfromfav", methods=["GET", "POST"])
+@login_required  # Assurez-vous que l'utilisateur est connecté
+def rmfromfav() -> str:
+    file_id = request.args.get(
+        "file_id"
+    )  # Obtenez l'ID du fichier à partir des paramètres de requête
+
+    if file_id:
+        file = File.query.get(file_id)
+        if file:
+            try:
+                # Vérifiez d'abord si le fichier est déjà dans la liste des favoris
+                if file in current_user.favorited_files:
+                    current_user.favorited_files.remove(file)
+                    db.session.commit()
+                    flash(
+                        "Fichier ajouté aux favoris avec succès.", FlashCategory.SUCCESS
+                    )
+                else:
+                    flash("Ce fichier n'est pas dans vos favoris.", FlashCategory.INFO)
+            except IntegrityError:
+                db.session.rollback()
+                flash(
+                    "Une erreur s'est produite lors de l'ajout du fichier aux favoris.",
+                    "error",
+                )
+        else:
+            flash("Fichier non trouvé.", FlashCategory.ERROR)
+
+    # Redirection ou affichage de la page, selon votre logique d'application
+    return redirect(
+        url_for("main.list_files")
+    )  # Redirigez vers la liste de fichiers après l'ajout aux favoris
