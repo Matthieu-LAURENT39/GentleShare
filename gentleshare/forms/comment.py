07af1b58a1036507c8e75ba7982d72f34dcebcb2
Matthieu LAURENT <matthieu.laurent69@proton.me>
2024-03-17 14:55:36 +0100

    Fix weird comment form validation

--- gentleshare/forms/comment.py
+++ gentleshare/forms/comment.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import SelectField, StringField, TextAreaField
+from wtforms import SelectField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
 
 class AddCommentForm(FlaskForm):
-    review = StringField(
+    review = TextAreaField(
         "Laisser un commentaire",
         validators=[
             DataRequired(),
@@ -15,7 +15,13 @@ class AddCommentForm(FlaskForm):
             ),
         ],
     )
-    rating = SelectField(
-        "Note",
-        choices=[(str(i), str(i)) for i in range(1, 11)]
-    )
+    rating = SelectField("Note", choices=[(str(i), str(i)) for i in range(1, 11)])
+
+    def validate_rating(self, field):
+        try:
+            rating = int(field.data)
+        except ValueError:
+            raise ValueError("La note doit être un nombre entier.")
+
+        if not (1 <= rating <= 10):
+            raise ValueError("La note doit être entre 1 et 10, inclusivement.")
